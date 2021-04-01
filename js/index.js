bee3611ec77c2f4194f06287dbfa834fc6e19d4e
Користувач <visockijtolik@gmail.com>
2021-04-01 12:17:26 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// menu burger
+
+let iconMenu = document.querySelector('.menu__icon');
+let menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+
+    iconMenu.addEventListener('click', function (e) {
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
